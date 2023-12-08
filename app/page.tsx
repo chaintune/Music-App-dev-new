5e1b8fd3a051addb6753bae6a79ada345faa0777
Abhijeet-2003 <sinhaabhijeet200@gmail.com>
2023-12-08 18:01:38 +0530

    home page complete

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,49 @@
-
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import CommunityPlaylist from '@components/communityPlaylist'
-import PlaySong from '@components/playSong'
-
-// import Image from 'next/image'
-// import { Inter } from 'next/font/google'
 import Navbar from '@components/navbar'
+import React from 'react'
+import PlaySong from '@components/playSong'
 import Sidebar from '@components/sidebar'
+import Community from '@components/community'
+import MixedCard from '@components/mixedCard'
+import NewLaunch from '@components/newLaunch'
+import RecPlay from '@components/recPlay'
+import {Cont, Left, Ele3, Ele31} from '@styles/Home/style'
 
-
-// const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const Home = () => {
   return (
-
-
-
-
-    <div className='bg-zinc-950' style={{ width: '100vw' , height: '100vh'}} >
-
-      <div className='overflow-none'>
+    <div>
         <Navbar />
-        <div className="flex justify-between" style={{padding: '0vw 2.8vw'}}>
-          <div>
-            <CommunityPlaylist />
-          </div>
+
+        <Cont>
+          <Left>
+
+            <NewLaunch />
+
+            <Ele3>
+              <Ele31></Ele31>
+
+              <RecPlay />
+            </Ele3>
+
+            <Community component={<MixedCard />} title='Communitites you can join' />
+            
+            <Community component={<MixedCard />} title='Artist Corner' />
+
+            <Community component={<MixedCard />} title='Recommended Playlists' />
+
+            <Community component={<MixedCard />} title='Trending Community Playlists' />
+
+            <Community component={<MixedCard />} title='Weekly Mixes' />
+
+          </Left>
+
           <Sidebar />
+        </Cont>
+
+        <div className='fixed left-0 right-0 bottom-0 flex justify-center items-center'>
+          <PlaySong />
         </div>
-      </div>
-
-
-      <div className='fixed left-0 right-0 bottom-0 flex justify-center items-center'>
-        <PlaySong />
-      </div>
     </div>
-
   )
 }
 
+export default Home
