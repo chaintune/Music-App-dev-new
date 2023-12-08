7775a815db0b869dc758ab1fe6efa4c7f8bc61a9
Abhijeet Sinha <108492066+Abhijeet-2003@users.noreply.github.com>
2023-12-08 18:08:36 +0530

    Update NavBar.tsx

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,16 +1,26 @@
 import arrow from '@assets/arrow.svg'
 import Image from 'next/image'
+import search from '@assets/search.svg'
 import {Nav, Left, Tab, Right, Wrap1, Wrap11, Wrap111, Wrap112, Wrap12, Wrap2, Wrap21, Wrap22} from "@styles/Navbar/style"
 
-const NavBar = () => {
+const Navbar = () => {
     return (
         <Nav>
             <Left>
                 <Tab>Home</Tab>
                 <Tab>Explore</Tab>
-                <Tab>Search</Tab>
+                <Image src={search} alt='' style={{width: '2.797vw', height: '2.797vw'}}  />
             </Left>
             <Right>
+                <Wrap1>
+                    <Wrap11>
+                        <Wrap111></Wrap111>
+                        <Wrap112>24.40</Wrap112>
+                    </Wrap11>
+                    <Wrap12>
+                        <Image src={arrow} alt="" style={{width: '0.9vw', height: '0.9vw'}}/>
+                    </Wrap12>
+                </Wrap1>
                 <Wrap1>
                     <Wrap11>
                         <Wrap111></Wrap111>
@@ -29,4 +39,4 @@ const NavBar = () => {
     );
 }
 
-export default NavBar;
+export default Navbar;
