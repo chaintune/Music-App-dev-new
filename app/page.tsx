8830ec97b8c9aace28cd2add0a9aad2966bec498
Abhijeet-2003 <sinhaabhijeet200@gmail.com>
2023-12-10 22:55:07 +0530

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useContext, useEffect } from 'react'
-import { Layout, Community, MixedCard, NewLaunch, RecPlay, DiscCard } from '@components'
+import { Layout, Community, MixedCard, NewLaunch, RecPlay, DiscCard, CurrentSong } from '@components'
 import { Cont, Left, Ele3, Ele31 } from '@styles/Home/style'
 import SongContext from '@context/dataContext'
 import DataContext from '@context/dataContext'
@@ -25,6 +25,7 @@ const Home = () => {
             <Cont>
                 <Left>
                     <NewLaunch/>
+                    <CurrentSong />
                     <Ele3>
                         <Ele31></Ele31>
                         <RecPlay/>
